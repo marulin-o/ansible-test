a04d0f103307102d0e3984be24f6d0b2a588373b
marulin-o <64163487+marulin-o@users.noreply.github.com>
2020-05-12 21:10:42 +0300

    Add files via upload

---
+++ roles/deploy-app/files/src/app/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.send('<h1>Hello World!</h1>');
+});
+app.listen(8081);
+console.log('Express server started successfully.');
